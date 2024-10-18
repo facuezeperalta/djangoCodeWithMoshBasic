d0e982199bc36e3c7f81afb0040dc018d28c8b24
Facundo Peralta <facundoperaltaezequiel@gmail.com>
2024-10-18 19:33:27 -0300

    models done.

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=255) #esto se traduce a la base de datos como varchar(255).
+    description = models.TextField() #no tiene un valor maximo de carácteres.
+    price = models.DecimalField(max_digits=6,decimal_places=2) #este es mejor usarlo que float ya que float tiene problemas de redondeo.
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now = True)
+
+class Customer(models.Model):
+    fist_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    birdth_date = models.DateField(null=True) #la fecha de nacimiento podrá dejarse en vacio.
+
+#LOS ID´S SE CREAN AUTOMÁTICAMENTE.
+
+
+
+
+
+    
+
+
